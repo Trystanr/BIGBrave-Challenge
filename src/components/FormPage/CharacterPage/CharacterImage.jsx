adc5e6e03e58547f2ba95d4903eaea02d720d374
Trystan Rivers <trystan.b@hotmail.com>
2021-01-10 21:37:08 +0200

    Character Generation Done
    Converted pants svg to React object to allow dynamic fill

--- src/components/FormPage/CharacterPage/CharacterImage.jsx
+++ src/components/FormPage/CharacterPage/CharacterImage.jsx
@@ -15,27 +15,67 @@ import oldMHead from '../../../images/character/old-m-head.png';
 
 import pants from '../../../images/character/pants.svg';
 
+import Logo from '../../../images/character/PantsItem';
+// import PantsItem from '../../../images/character/PantsItem';
+
+
 import logo from '../../../images/icon.png';
 
 export default function CharacterImage(props) {
 
+	function calcAge(dob) {
+		let birthDate = new Date(dob);
+		let age = new Date(Date.now() - birthDate.getTime());
+
+		return Math.abs(age.getUTCFullYear() - 1970);
+	}
+
+	function getHead() {
+		let hGender = props.values.gender;
+
+		// Toss the coin
+		if (props.values.gender == "other") {
+			console.log(Math.round(Math.random() * 1));
+			if (Math.round(Math.random() * 1) == 0) {
+				hGender = "female";
+			} else {
+				hGender = "male";
+			}
+		}
+
+		if (hGender == "female") {
+			if (calcAge(props.values.dob) < 60) {
+				return fHead;
+			} else {
+				return oldFHhead;
+			}
+		} else if (hGender == "male") {
+			if (calcAge(props.values.dob) < 60) {
+				return mHead;
+			} else {
+				return oldMHead;
+			}
+		}
+	}
+
 	return (
 		<React.Fragment>
-			<h1>Character here sir {props.values.firstName}</h1>
+			<h1>{props.values.firstName} {props.values.lastName}</h1>
 			<div id="characterCont">
-				<img id="legs" src={pants} />
+				{/*{<img id="legs" src= {pants} />}*/}
+				{<Logo fill="#fff" className="legs"/>}
 				<img id="torso" src=
 				{
 				    {
 				    	'chef': chef,
 				      	'developer': developer,
-				      	'socialMedia': socialMedia,
+				      	'social-media': socialMedia,
 				      	'yoga': yoga
 				    }[props.values.occupation]
 				}
-				 />
-				{/*<img id="torso" src={chef} />*/}
-				<img id="head" src={fHead} />
+				/>
+				<img id="head" src={getHead()} />
+				
 			</div>
 		</React.Fragment>
 	);
